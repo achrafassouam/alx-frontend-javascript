a1b30005435a7480692ffc33c37a3d3e9224b02a
“Achraf <achrafassouam@gmail.com>
2024-07-25 18:43:28 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -16,29 +16,35 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
-  }
 
   get length() {
     return this._length;
-  }
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = value;
   }
 
   get students() {
     return this._students;
   }
-  set students(value) {
-    if (typeof value !== 'string') {
+  
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  
+  set students(students) {
+    if (typeof students !== 'string') {
       throw new TypeError('Students must be an array of strings');
     }
+    this._students = students;
   }
 }

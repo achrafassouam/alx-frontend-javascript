6b5fc09c1bc95fee17c3dcb8582e743766bf34df
“Achraf <achrafassouam@gmail.com>
2024-07-25 19:40:52 +0100

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+/*
+Implement a class named Airport:
+
+Constructor attributes:
+name (String)
+code (String)
+Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+The default string description of the class should return the airport code (example below).
+*/
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}

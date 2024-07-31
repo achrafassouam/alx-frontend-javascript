45cd09d9030be5a301e0103ccee380449c4e4b00
“Achraf <achrafassouam@gmail.com>
2024-07-31 20:30:04 +0100

    updating main.ts in task 0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,20 +1,3 @@
-/*
-Write an interface named Student that accepts the following elements: 
-	firstName(string),
-	lastName(string),
-	age(number),
-	location(string)
-
-Create two students,
-	create an array named studentsList containing the two variables
-
-Using Vanilla Javascript, 
-	render a table and for each elements in the array,
-	append a new row to the table
-
-Each row should contain the first name of the student and the location
-*/
-
 interface Student {
   firstName: string;
   lastName: string;

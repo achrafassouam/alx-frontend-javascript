65bc686ee973961f0313058c99d273c6229e4e12
“Achraf <achrafassouam@gmail.com>
2024-07-31 21:33:20 +0100

    adding main.ts in task_4

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,36 @@
+/// <reference path="subjects/Teacher.ts" />
+/// <reference path="subjects/Subject.ts" />
+/// <reference path="subjects/Cpp.ts" />
+/// <reference path="subjects/React.ts" />
+/// <reference path="subjects/Java.ts" />
+
+/// <reference path="subjects/Cpp.ts" />import { Subjects as JavaSubjects } from './subjects/Java';
+/// <reference path="subjects/React.ts" />
+// Create and export constants for subjects
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+// Create and export a Teacher object with experienceTeachingC = 10
+export const cTeacher: Subjects.Teacher = {
+  firstName: "Bobby",
+  lastName: "Wine",
+  experienceTeachingC: 10,
+};
+
+// For Cpp subject
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java subject
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React subject
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
